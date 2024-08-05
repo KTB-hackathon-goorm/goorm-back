d41e882efe1df653826d4e31dfa1eb508a98f548
49EHyeon42 <49ehyeon42@gmail.com>
2024-08-06 08:49:18 +0900

    init: 프로젝트 시작

---
+++ src/main/java/dev/ktb_hackathon/goorm/GoormApplication.java
@@ -0,0 +1,12 @@
+package dev.ktb_hackathon.goorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormApplication.class, args);
+    }
+}
