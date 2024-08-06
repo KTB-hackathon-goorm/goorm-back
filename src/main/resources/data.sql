ada4f727bc3a532cf92e0c528456f02f3a86c660
49EHyeon42 <49ehyeon42@gmail.com>
2024-08-06 12:01:26 +0900

    chore: 테스트용으로 초기 사용자 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member (id, email, password, nickname) VALUES (9223372036854775807, 'user1@example.com', 'password1', 'user1');
+INSERT INTO member (id, email, password, nickname) VALUES (9223372036854775806, 'user2@example.com', 'password2', 'user2');
+INSERT INTO member (id, email, password, nickname) VALUES (9223372036854775805, 'user3@example.com', 'password3', 'user3');
